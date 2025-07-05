fa4a027d973cc3afaa4e71fc393cce6068bee79f
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2025-07-05 14:26:10 -0500

    initial commit

---
+++ lib/App/dbitemplater.pm
@@ -0,0 +1,105 @@
+package App::dbitemplater;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+App::dbitemplater - The great new App::dbitemplater!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use App::dbitemplater;
+
+    my $foo = App::dbitemplater->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-app-dbitemplater at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-dbitemplater>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::dbitemplater
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=App-dbitemplater>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/App-dbitemplater>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/App-dbitemplater>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2025 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 3, June 2007
+
+
+=cut
+
+1; # End of App::dbitemplater
