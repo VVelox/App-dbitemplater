fc6423945881e04db467ad62a805b331e89fe3b4
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2025-07-07 08:11:45 -0500

    bless self and call process now

--- lib/App/dbitemplater.pm
+++ lib/App/dbitemplater.pm
@@ -102,11 +102,12 @@ sub new {
 		'END_TAG'    => undef,
 		'config'     => \%args,
 	};
+	bless $self;
 
 	# ensure that all required args are present and that any arts that are present have a
 	# ref of ""
 	foreach my $item (@possible_args) {
-		if ( defined($required_args->{$item}) && !defined( $args{$item} ) ) {
+		if ( defined( $required_args->{$item} ) && !defined( $args{$item} ) ) {
 			die( '$args{"' . $item . '"} is not defined and is required' );
 		}
 		if ( defined( $args{$item} ) && ref( $args{$item} ) ne '' ) {
